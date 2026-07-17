7cc451baba07808dcdd890546f07e2bc4c26b193
Juergen <juergenschaeble2@gmail.com>
2026-07-17 20:52:17 +0200

    v4.6.10: Name vereinheitlicht auf 'Divergenzmaschine' (Titel + Manifest), id für Parallel-Install bleibt

--- migration-ts/dist/sw.js
+++ migration-ts/dist/sw.js
@@ -1,5 +1,5 @@
-/* Divergenzmaschine V4.2 – Service Worker */
-const CACHE = 'divergenzmaschine-ts-v32';
+/* Divergenzmaschine – Service Worker */
+const CACHE = 'divergenzmaschine-ts-v33';
 const PRECACHE = [
   './',
   './index.html',
