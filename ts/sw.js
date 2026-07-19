27fc1a380b08898b2f3021b2428bd8fedfec2e26
Juergen <juergenschaeble2@gmail.com>
2026-07-19 14:33:26 +0200

    Deploy ts v4.11.0: Werkstatt-Projekte

--- ts/sw.js
+++ ts/sw.js
@@ -1,5 +1,5 @@
 /* Divergenzmaschine – Service Worker (offline-fähig) */
-const CACHE = 'divergenzmaschine-ts-v50';
+const CACHE = 'divergenzmaschine-ts-v51';
 const PRECACHE = [
   './',
   './index.html',
