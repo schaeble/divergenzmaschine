524d3084a871b3375f4f8b44de266709ce63eb03
Juergen <juergenschaeble2@gmail.com>
2026-07-29 18:06:37 +0200

    Hilfe auf aktuellen Stand: neue Abschnitte 'Einspeisungen faerben & Passagen-Austausch' und 'Preset 2.0 (Dramaturgie)'; Struktur-Zeile um Dramaturgie ergaenzt; Schatzkammer- und Wortbank-Tab-Beschreibungen aktualisiert (Form-Filter, Regeln, Per KI aktualisieren) (v4.75.0)

--- migration-ts/public/sw.js
+++ migration-ts/public/sw.js
@@ -1,5 +1,5 @@
 /* Divergenzmaschine – Service Worker (offline-fähig) */
-const CACHE = 'divergenzmaschine-ts-4.74.0';
+const CACHE = 'divergenzmaschine-ts-4.75.0';
 const PRECACHE = [
   './',
   './index.html',
