dd9ecb584d438d88d0427098fa12ff777f76b564
Juergen Schaeble <juergenschaeble2@gmail.com>
2026-08-07 21:21:37 +0200

    Hilfe: Rekombination als Standard beschrieben, mit den Vergleichszahlen (v4.180.1)

--- migration-ts/dist/sw.js
+++ migration-ts/dist/sw.js
@@ -1,5 +1,5 @@
 /* Divergenzmaschine – Service Worker (offline-fähig) */
-const CACHE = 'divergenzmaschine-ts-4.180.0';
+const CACHE = 'divergenzmaschine-ts-4.180.1';
 const PRECACHE = [
   './',
   './index.html',
